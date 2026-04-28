de9286c9051f4d20f871fb6652cd600914f92691
Aydyn <darhanlyajdyn02@gmail.com>
2026-04-28 06:03:06 +0500

    TSIS 1 - Phonebook

---
+++ TSIS/TSIS01/config.py
@@ -0,0 +1,13 @@
+# Configuration file for database connection settings
+# These variables are used in connect.py to establish a connection to PostgreSQL
+
+# Name of the database to connect to
+DB_NAME = "phonebook_tsis"
+# Database username (PostgreSQL user)
+USER = "postgres"
+# Password for the PostgreSQL user
+PASSWORD = "12345678"
+# Host address of the database (localhost means your own computer)
+HOST = "localhost"
+# Port number for PostgreSQL (default is 5432)
+PORT = "5432"
